931f186a40bc6990eb7dafe5210263e8f0796c99
Grégoire Seux <g.seux@criteo.com>
2020-11-08 14:41:19 +0100

    Solve day2
    Change-Id: I7f8b58c3b1f742f61c30093f0e9c2a62d129714a

--- days/day2.rb
+++ days/day2.rb
@@ -3,4 +3,28 @@
 require 'aoc'
 
 class Day2 < Day
+  def solve_part1
+    lines = input.split("\n").map { |line| line.split(' ').map(&:to_i) }
+
+    lines.map do |line|
+      line.max - line.min
+    end.sum
+  end
+
+  def solve_part2
+    lines = input.split("\n").map { |line| line.split(' ').map(&:to_i) }
+
+    lines.map do |line|
+      solve_line(line)
+    end.sum
+
+  end
+
+  def solve_line(line)
+    line.combination(2).filter_map do |a, b|
+      a, b = [a, b].minmax
+      q, r = b.divmod(a)
+      q if r.zero?
+    end.first
+  end
 end
