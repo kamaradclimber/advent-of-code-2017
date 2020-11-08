feae35d9366fd7a74e75a09b3a2656d110e5f8ca
Grégoire Seux <g.seux@criteo.com>
2020-11-08 18:49:29 +0100

    Solve day 4
    Change-Id: I24294287331574fb8e09f05af28ce9a8c574408a

--- days/day4.rb
+++ days/day4.rb
@@ -3,4 +3,27 @@
 require 'aoc'
 
 class Day4 < Day
+  def solve_part1
+    input.split("\n").select { |phrase| part1_valid?(phrase) }.count
+  end
+
+  def part1_valid?(passphrase)
+    passphrase.split(' ').tally.all? do |_word, count|
+      count == 1
+    end
+  end
+
+  def solve_part2
+    input.split("\n").select { |phrase| part2_valid?(phrase) }.count
+  end
+
+  def part2_valid?(passphrase)
+    passphrase
+      .split(' ')
+      .map { |word| word.chars.sort }
+      .tally
+      .all? do |_word, count|
+      count == 1
+    end
+  end
 end
