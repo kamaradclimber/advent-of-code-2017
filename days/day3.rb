56a5ca06f0a67ca03befcd19f04ddafd700cee76
Grégoire Seux <g.seux@criteo.com>
2020-11-08 15:59:04 +0100

    Solve day3 part1
    Change-Id: I71885f34d9d93aaa37e85ad6e0e9947affa624e0

--- days/day3.rb
+++ days/day3.rb
@@ -3,4 +3,51 @@
 require 'aoc'
 
 class Day3 < Day
+  # south-east number of the grid is (1 + 2n)^2 where n is the position of this number (n, -n) on the grid
+  #
+  def solve_part1
+    x, y = coords(input)
+    x.abs + y.abs
+  end
+
+  def coords(input)
+    # now we have the highest south-east corner that is < input.
+    ring = (0..).find do |n|
+      se(n + 1) > input
+    end + 1
+    ring -= 1 if input == se(ring - 1)
+    # puts "#{input} belongs to ring #{ring}, se value is #{se(ring)}"
+    case input
+    when se(ring)
+      [ring, -ring]
+    when (se(ring - 1)..ne(ring))
+      [ring, -ring + input - se(ring - 1)]
+    when (ne(ring)..nw(ring))
+      [ring - (input - ne(ring)), ring]
+    when (nw(ring)..sw(ring))
+      [-ring, ring - (input - nw(ring))]
+    else
+      [-ring + (input - sw(ring)), -ring]
+    end
+  end
+
+  # @return [Integer] value at position (n, -n)
+  def se(n)
+    (2 * n + 1)**2
+  end
+
+  # @return [Integer] value at position (n, n)
+  def ne(n)
+    se(n - 1) + 2 * n
+  end
+
+  # @return [Integer] value at position (-n, n)
+  def nw(n)
+    ne(n) + 2 * n
+  end
+
+  # @return [Integer] value at position (-n, -n)
+  def sw(n)
+    nw(n) + 2 * n
+  end
 end
--- spec/day3_spec.rb
+++ spec/day3_spec.rb
@@ -7,17 +7,46 @@ RSpec.describe Day3 do
   describe 'part 1' do
     let(:part) { 1 }
     {
+      1 => 0,
+      12 => 3,
+      23 => 2,
+      1024 => 31
     }.each do |example, solution|
       context "example #{example[0..15]}" do
         let(:input) { example }
-        pending "works on example #{example}" do
+        it "works on example #{example}" do
           expect(subject.solve).to eq(solution)
         end
       end
     end
 
+    it 'computes corner_values correctly' do
+      expect(subject.se(1)).to eq(9)
+      expect(subject.ne(1)).to eq(3)
+      expect(subject.se(2)).to eq(25)
+      expect(subject.ne(2)).to eq(13)
+      expect(subject.nw(2)).to eq(17)
+      expect(subject.sw(2)).to eq(21)
+      expect(subject.ne(3)).to eq(31)
+    end
+
+    it 'computes coords correctly' do
+      expect(subject.coords(1)).to eq([0, 0])
+      expect(subject.coords(9)).to eq([1, -1])
+      expect(subject.coords(10)).to eq([2, -1])
+      expect(subject.coords(11)).to eq([2, 0])
+      expect(subject.coords(12)).to eq([2, 1])
+      expect(subject.coords(13)).to eq([2, 2])
+      expect(subject.coords(14)).to eq([1, 2])
+      expect(subject.coords(15)).to eq([0, 2])
+      expect(subject.coords(17)).to eq([-2, 2])
+      expect(subject.coords(20)).to eq([-2, -1])
+      expect(subject.coords(23)).to eq([0, -2])
+      expect(subject.coords(28)).to eq([3, 0])
+    end
+
     context 'real input' do
-      pending 'finds a solution for part1' do
+      it 'finds a solution for part1' do
         solution = subject.solve
         puts "Solution for part 1 is #{solution}"
       end
@@ -45,5 +74,6 @@ RSpec.describe Day3 do
   end
 
   let(:input) do
+    312051
   end
 end
