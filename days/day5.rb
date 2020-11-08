99c2d1d72417bdb348be2b7fbf4e13f8a1d0bd81
Grégoire Seux <g.seux@criteo.com>
2020-11-08 19:00:12 +0100

    Solve day5
    Change-Id: I03c910ed811ac9055ed2b393b0314afc1ba29602

--- days/day5.rb
+++ days/day5.rb
@@ -3,4 +3,29 @@
 require 'aoc'
 
 class Day5 < Day
+  def solve_part1
+    jump_list = input.split(' ').map(&:to_i)
+    index = 0
+    step_count = 0
+    while index < jump_list.size && index >= 0
+      offset = jump_list[index]
+      jump_list[index] += 1
+      index += offset
+      step_count += 1
+    end
+    step_count
+  end
+
+  def solve_part2
+    jump_list = input.split(' ').map(&:to_i)
+    index = 0
+    step_count = 0
+    while index < jump_list.size && index >= 0
+      offset = jump_list[index]
+      jump_list[index] += offset >= 3 ? -1 : 1
+      index += offset
+      step_count += 1
+    end
+    step_count
+  end
 end
