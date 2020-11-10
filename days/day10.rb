b7f087ae854e7c856d2579c9b7f91d59c7db423b
Grégoire Seux <g.seux@criteo.com>
2020-11-10 21:45:26 +0100

    Resolve day 10
    Change-Id: I5b4227deca56f98fcf9ca9cef4d57f820c327b44

--- days/day10.rb
+++ days/day10.rb
@@ -1,6 +1,75 @@
 # frozen_string_literal: true
 
 require 'aoc'
+require 'pry'
 
 class Day10 < Day
+  def initialize(input:, part:, circular_list_size: 256)
+    super(input: input, part: part)
+    @circular_list_size = circular_list_size
+    @list = (0..circular_list_size - 1).to_a
+    @current_position = 0
+    @skip_size = 0
+  end
+
+  def solve_part1
+    lengthes = input.split(',').map(&:strip).map(&:to_i)
+    round(lengthes)
+    @list[0] * @list[1]
+  end
+
+  def solve_part2
+    suffix = [17, 31, 73, 47, 23]
+    lengthes = input.strip.chars.map(&:ord) + suffix
+    64.times do
+      round(lengthes)
+      raise 'Assertion false: size is not the same' unless @circular_list_size == @list.size
+    end
+    dense_hash
+  end
+
+  def dense_hash
+    hexes = @list.each_slice(16).map do |slice|
+      slice.reduce do |memo, el|
+        memo ^ el
+      end
+    end
+    hexes.map { |int| format('%02x', int) }.join # rubocop:disable Style/FormatStringToken
+  end
+
+  def round(lengthes)
+    lengthes.each do |length|
+      apply(length)
+    end
+  end
+
+  def apply(length)
+    reverse(@current_position, length)
+    @current_position = index(@current_position + length + @skip_size)
+    @skip_size += 1
+  end
+
+  def inspect
+    "#{@list.join(',')}\npos: #{@current_position} skip: #{@skip_size}"
+  end
+
+  def reverse(start, length)
+    (length / 2).times do |i|
+      a_index = index(start + i)
+      b_index = index(start + length - 1 - i)
+      a = @list[a_index]
+      b = @list[b_index]
+      @list[a_index] = b
+      @list[b_index] = a
+    end
+  end
+
+  # return an index within the list
+  def index(i)
+    if i < @list.size
+      i
+    else
+      (i % @list.size)
+    end
+  end
 end
--- spec/day10_spec.rb
+++ spec/day10_spec.rb
@@ -7,17 +7,21 @@ RSpec.describe Day10 do
   describe 'part 1' do
     let(:part) { 1 }
     {
+      '3, 4, 1, 5' => 12
     }.each do |example, solution|
       context "example #{example[0..15]}" do
+        subject do
+          described_class.new(input: input, part: part, circular_list_size: 5)
+        end
         let(:input) { example }
-        pending "works on example #{example}" do
+        it "works on example #{example}" do
           expect(subject.solve).to eq(solution)
         end
       end
     end
 
     context 'real input' do
-      pending 'finds a solution for part1' do
+      it 'finds a solution for part1' do
         solution = subject.solve
         puts "Solution for part 1 is #{solution}"
       end
@@ -27,17 +31,21 @@ RSpec.describe Day10 do
   describe 'part 2' do
     let(:part) { 2 }
     {
+      '' => 'a2582a3a0e66e6e86e3812dcb672a272',
+      'AoC 2017' => '33efeb34ea91902bb2f59c9920caa6cd',
+      '1,2,3' => '3efbe78a8d82f29979031a4aa0b16a9d',
+      '1,2,4' => '63960835bcdc130f0b66d7ff4f6a5a8e'
     }.each do |example, solution|
       context "example #{example[0..15]}" do
         let(:input) { example }
-        pending "works on example #{example}" do
+        it "works on example #{example}" do
           expect(subject.solve).to eq(solution)
         end
       end
     end
 
     context 'real input' do
-      pending 'finds a solution for part2' do
+      it 'finds a solution for part2' do
         solution = subject.solve
         puts "Solution for part 2 is #{solution}"
       end
@@ -45,5 +53,6 @@ RSpec.describe Day10 do
   end
 
   let(:input) do
+    '165,1,255,31,87,52,24,113,0,91,148,254,158,2,73,153'
   end
 end
