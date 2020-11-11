e54320b4753031cd881988e0528bed3fd1c0de71
Grégoire Seux <kamaradclimber@gmail.com>
2020-11-11 15:29:22 +0100

    Resolve day 13 in 3 minutes 🐌
    There is likely a small optimization to avoid checking all solutions
    sequentially
    
    Change-Id: I4d90901b893b5d5421eda4f62ef571b84685af8d

--- days/day13.rb
+++ days/day13.rb
@@ -3,4 +3,97 @@
 require 'aoc'
 
 class Day13 < Day
+  def solve_part1
+    parse_input
+    traverse_firewall(@firewall).sum
+  end
+
+  def solve_part2
+    parse_input
+    delay = 0
+
+    loop do
+      puts "Delay #{delay}" if delay % 10000 == 0
+      firewall_copy = @firewall.transform_values(&:dup)
+      break if traverse_firewall(firewall_copy).empty?
+
+      @firewall.values.each(&:tick)
+      delay += 1
+    end
+
+    delay
+  end
+
+  def print_firewall(firewall, cur_layer: nil)
+    layers = firewall.keys.max + 1
+    max_line = firewall.values.map(&:range).max
+    max_line.times do |line|
+      layers.times do |layer|
+        chars = if firewall[layer]
+                  if firewall[layer].position == line
+                    '[S] '
+                  elsif firewall[layer].range > line
+                    '[ ]'
+                  else
+                    '... '
+                  end
+                else
+                  '... '
+                end
+        chars.gsub('[', '(').gsub(']', ')') if cur_layer == layer
+        print chars
+      end
+      puts ''
+    end
+  end
+
+  # @param firewall [Hash<int, Scanner>]
+  # @return [Array<Integer>] severities of the trip
+  def traverse_firewall(firewall)
+    packet_layer = -1
+    severities = []
+    max_layer = firewall.keys.max
+    until packet_layer > max_layer
+      packet_layer += 1
+      severities << packet_layer * firewall[packet_layer].range if firewall[packet_layer]&.position == 0
+      break if part2? && severities.any?
+
+      firewall.values.each(&:tick)
+    end
+    severities
+  end
+
+  def parse_input
+    @firewall = input.split("\n").map do |line|
+      raise "Impossible to parse line: #{line}" unless line =~ /^(\d+): (\d+)$/
+
+      [Regexp.last_match(1).to_i, Scanner.new(depth: Regexp.last_match(1).to_i, range: Regexp.last_match(2).to_i)]
+    end.to_h
+  end
+
+  class Scanner
+    attr_reader :range, :position
+
+    # @param depth [Integer] layer of this scanner
+    # @param range [Integer] depth of the scanner
+    # @param initial_position [Integer]
+    # @param inital_direction [Integer]
+    def initialize(depth:, range:, initial_position: 0, initial_direction: 1)
+      @depth = depth
+      @range = range
+      @position = initial_position
+      @direction = initial_direction
+    end
+
+    def tick
+      @position += @direction
+      @direction *= -1 if @position == @range - 1 || @position == 0
+    end
+
+    def dup
+      Scanner.new(depth: @depth, range: range,
+                  initial_position: position,
+                  initial_direction: @direction)
+    end
+  end
 end
--- spec/day13_spec.rb
+++ spec/day13_spec.rb
@@ -7,17 +7,21 @@ RSpec.describe Day13 do
   describe 'part 1' do
     let(:part) { 1 }
     {
+      '0: 3
+1: 2
+4: 4
+6: 4' => 24
     }.each do |example, solution|
       context "example #{example[0..15]}" do
         let(:input) { example }
-        pending "works on example #{example}" do
+        it "works on example #{example}" do
           expect(subject.solve).to eq(solution)
         end
       end
     end
 
     context 'real input' do
-      pending 'finds a solution for part1' do
+      it 'finds a solution for part1' do
         solution = subject.solve
         puts "Solution for part 1 is #{solution}"
       end
@@ -27,17 +31,21 @@ RSpec.describe Day13 do
   describe 'part 2' do
     let(:part) { 2 }
     {
+      '0: 3
+1: 2
+4: 4
+6: 4' => 10
     }.each do |example, solution|
       context "example #{example[0..15]}" do
         let(:input) { example }
-        pending "works on example #{example}" do
+        it "works on example #{example}" do
           expect(subject.solve).to eq(solution)
         end
       end
     end
 
     context 'real input' do
-      pending 'finds a solution for part2' do
+      it 'finds a solution for part2' do
         solution = subject.solve
         puts "Solution for part 2 is #{solution}"
       end
@@ -45,5 +53,48 @@ RSpec.describe Day13 do
   end
 
   let(:input) do
+    %(0: 3
+1: 2
+2: 4
+4: 4
+6: 5
+8: 6
+10: 6
+12: 8
+14: 6
+16: 6
+18: 9
+20: 8
+22: 8
+24: 8
+26: 12
+28: 8
+30: 12
+32: 12
+34: 12
+36: 10
+38: 14
+40: 12
+42: 10
+44: 8
+46: 12
+48: 14
+50: 12
+52: 14
+54: 14
+56: 14
+58: 12
+62: 14
+64: 12
+66: 12
+68: 14
+70: 14
+72: 14
+74: 17
+76: 14
+78: 18
+84: 14
+90: 20
+92: 14)
   end
 end
