b92bb5cc21d4394b25b344ab56d3f35382b63b40
Grégoire Seux <kamaradclimber@gmail.com>
2020-11-14 14:21:41 +0100

    Resolve day 18
    Change-Id: I2cf91757bddbab9664a2ef888e36cf713022e75d

--- days/day18.rb
+++ days/day18.rb
@@ -3,4 +3,118 @@
 require 'aoc'
 
 class Day18 < Day
+  def solve_part1 # rubocop:disable Metrics/MethodLength
+    @registers = Hash.new(0)
+    @last_played = nil
+    instructions = input.split("\n")
+    i = 0
+    while i >= 0 && i < instructions.size
+      instruction = instructions[i]
+      case instruction
+      when /snd (\w+)/
+        @last_played = value_of(Regexp.last_match(1))
+      when /set (\w+) (\w+)/
+        @registers[Regexp.last_match(1)] = value_of(Regexp.last_match(2))
+      when /add (\w+) (-?\w+)/
+        @registers[Regexp.last_match(1)] += value_of(Regexp.last_match(2))
+      when /mul (\w+) (-?\w+)/
+        @registers[Regexp.last_match(1)] = value_of(Regexp.last_match(1)) * value_of(Regexp.last_match(2))
+      when /mod (\w+) (\w+)/
+        @registers[Regexp.last_match(1)] %= value_of(Regexp.last_match(2))
+      when /rcv (\w+)/
+        if value_of(Regexp.last_match(1)) > 0
+          @recovered = @last_played
+          break
+        end
+      when /jgz (\w+) (-?\w+)/
+        if value_of(Regexp.last_match(1)) > 0
+          i += value_of(Regexp.last_match(2))
+          next
+        end
+      else
+        raise NotImplementedError, "#{instruction} is not handled"
+      end
+      i += 1
+    end
+    @recovered
+  end
+
+  def value_of(object)
+    if object.to_i.to_s == object
+      object.to_i
+    else
+      @registers[object]
+    end
+  end
+
+  def solve_part2
+    q1 = Queue.new
+    q2 = Queue.new
+    instructions = input.split("\n")
+    p0 = Program.new(q1, q2, instructions, id: 0)
+    p1 = Program.new(q2, q1, instructions, id: 1)
+    loop do
+      p0.run
+      p1.run
+      break if q1.empty? && q2.empty?
+    end
+    p1.sent
+  end
+
+  class Program
+    attr_reader :sent, :instructions
+
+    def initialize(send_queue, recv_queue, instructions, id:)
+      @registers = Hash.new(0)
+      @registers['p'] = id
+      @send_queue = send_queue
+      @recv_queue = recv_queue
+      @instructions = instructions
+      @sent = 0
+      @i = 0
+      @id = id
+    end
+
+    def value_of(object)
+      if object.to_i.to_s == object
+        object.to_i
+      else
+        @registers[object]
+      end
+    end
+
+    def run
+      while @i >= 0 && @i < instructions.size
+        instruction = instructions[@i]
+        case instruction
+        when /snd (\w+)/
+          @send_queue << value_of(Regexp.last_match(1))
+          @sent += 1
+        when /set (\w+) (\w+)/
+          @registers[Regexp.last_match(1)] = value_of(Regexp.last_match(2))
+        when /add (\w+) (-?\w+)/
+          @registers[Regexp.last_match(1)] += value_of(Regexp.last_match(2))
+        when /mul (\w+) (-?\w+)/
+          @registers[Regexp.last_match(1)] = value_of(Regexp.last_match(1)) * value_of(Regexp.last_match(2))
+        when /mod (\w+) (\w+)/
+          @registers[Regexp.last_match(1)] %= value_of(Regexp.last_match(2))
+        when /rcv (\w+)/
+          begin
+            @registers[Regexp.last_match(1)] = @recv_queue.pop(true)
+          rescue ThreadError
+            # there were no message, let's yield for now
+            return
+          end
+        when /jgz (\w+) (-?\w+)/
+          if value_of(Regexp.last_match(1)) > 0
+            @i += value_of(Regexp.last_match(2))
+            next
+          end
+        else
+          raise NotImplementedError, "#{instruction} is not handled"
+        end
+        @i += 1
+      end
+    end
+  end
 end
--- spec/day18_spec.rb
+++ spec/day18_spec.rb
@@ -7,17 +7,27 @@ RSpec.describe Day18 do
   describe 'part 1' do
     let(:part) { 1 }
     {
+      'set a 1
+add a 2
+mul a a
+mod a 5
+snd a
+set a 0
+rcv a
+jgz a -1
+set a 1
+jgz a -2' => 4
     }.each do |example, solution|
       context "example #{example[0..15]}" do
         let(:input) { example }
-        pending "works on example #{example}" do
+        it "works on example #{example}" do
           expect(subject.solve).to eq(solution)
         end
       end
     end
 
     context 'real input' do
-      pending 'finds a solution for part1' do
+      it 'finds a solution for part1' do
         solution = subject.solve
         puts "Solution for part 1 is #{solution}"
       end
@@ -27,17 +37,24 @@ RSpec.describe Day18 do
   describe 'part 2' do
     let(:part) { 2 }
     {
+      'snd 1
+snd 2
+snd p
+rcv a
+rcv b
+rcv c
+rcv d' => 3
     }.each do |example, solution|
       context "example #{example[0..15]}" do
         let(:input) { example }
-        pending "works on example #{example}" do
+        it "works on example #{example}" do
           expect(subject.solve).to eq(solution)
         end
       end
     end
 
     context 'real input' do
-      pending 'finds a solution for part2' do
+      it 'finds a solution for part2' do
         solution = subject.solve
         puts "Solution for part 2 is #{solution}"
       end
@@ -45,5 +62,46 @@ RSpec.describe Day18 do
   end
 
   let(:input) do
+    %(set i 31
+set a 1
+mul p 17
+jgz p p
+mul a 2
+add i -1
+jgz i -2
+add a -1
+set i 127
+set p 826
+mul p 8505
+mod p a
+mul p 129749
+add p 12345
+mod p a
+set b p
+mod b 10000
+snd b
+add i -1
+jgz i -9
+jgz a 3
+rcv b
+jgz b -1
+set f 0
+set i 126
+rcv a
+rcv b
+set p a
+mul p -1
+add p b
+jgz p 4
+snd a
+set a b
+jgz 1 3
+snd b
+set f 1
+add i -1
+jgz i -11
+snd a
+jgz f -16
+jgz a -19)
   end
 end
